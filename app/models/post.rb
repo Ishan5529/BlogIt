d76c6d679928ff485f9c90e3045b5669e39d6f63
BigBinary <bigbinary@BigBinarys-MacBook-Pro.local>
2025-06-04 14:30:46 +0530

    Added model validation to ensure max length and presence of title and description and, ensure is_bloggable is either true or false

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  MAX_TITLE_LENGTH = 125
+  MAX_DESCRIPTION_LENGTH = 10000
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
+  validates :description, presence: true, length: { maximum: MAX_DESCRIPTION_LENGTH }
+  validates :is_bloggable, inclusion: { in: [true, false] }
   validates :upvotes, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :downvotes, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
